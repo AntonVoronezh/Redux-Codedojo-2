221abf1627a1689d31831adb78f2b669108bc380
AntonVoronezh <pstut@mail.ru>
2019-04-13 18:47:26 +0300

    19

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,40 +1,72 @@
 import { ADD_TODO, DELETE_TODO, TOGGLE_TODO, EDIT_TODO } from '../actions/index';
 
-export default function reducer(state = [], action) {
+function todoReducer(state = {}, action) {
 	switch (action.type) {
 		case ADD_TODO:
-			const todo = {
+			return {
 				id: action.id,
 				title: action.title,
 				completed: false,
 			};
-			return [...state, todo];
+
+		case TOGGLE_TODO:
+			if (state.id !== action.id) {
+				return state;
+			}
+
+			return Object.assign({}, state, {
+				completed: !state.completed,
+			});
+
+		case EDIT_TODO:
+			if (state.id !== action.id) {
+				return state;
+			}
+
+			return Object.assign({}, state, {
+				title: action.title,
+			});
+	}
+}
+
+export default function reducer(state = [], action) {
+	switch (action.type) {
+		case ADD_TODO:
+			// const todo = {
+			// 	id: action.id,
+			// 	title: action.title,
+			// 	completed: false,
+			// };
+			// return [...state, todo];
+			return [...state, todoReducer(undefined, action)];
 
 		case DELETE_TODO:
 			const index = state.findIndex(todo => todo.id === action.id);
 			return [...state.slice(0, index), ...state.slice(index + 1)];
 
-        case TOGGLE_TODO:
-        return state.map(todo => {
-            if (todo.id !== action.id) {
-                return todo;
-            }
+		case TOGGLE_TODO:
+			// return state.map(todo => {
+			// 	if (todo.id !== action.id) {
+			// 		return todo;
+			// 	}
 
-            return Object.assign({}, todo, {
-                completed: !todo.completed
-            });
-        });
+			// 	return Object.assign({}, todo, {
+			// 		completed: !todo.completed,
+			// 	});
+			// });
+			return state.map(todo => todoReducer(todo, action));
 
 		case EDIT_TODO:
-        return state.map(todo => {
-            if (todo.id !== action.id) {
-                return todo;
-            }
+			// return state.map(todo => {
+			// 	if (todo.id !== action.id) {
+			// 		return todo;
+			// 	}
 
-            return Object.assign({}, todo, {
-                title: action.title
-            });
-        });
+			// 	return Object.assign({}, todo, {
+			// 		title: action.title,
+			// 	});
+			// });
+			return state.map(todo => todoReducer(todo, action));
 
 		default:
 			return state;
