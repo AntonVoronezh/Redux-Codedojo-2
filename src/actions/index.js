f7f449e683ebb6abf3bac678518518cd20237ba4
AntonVoronezh <pstut@mail.ru>
2019-04-13 18:52:15 +0300

    20#1

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,14 @@ export const ADD_TODO = 'ADD_TODO';
 export const DELETE_TODO = 'DELETE_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
 export const EDIT_TODO = 'EDIT_TODO';
+
+let nextId = 5;
+
+//actionsCrietors
+export function addTodo(title) {
+    return {
+        type: ADD_TODO,
+        id: nextId++,
+        title
+    };
+}
