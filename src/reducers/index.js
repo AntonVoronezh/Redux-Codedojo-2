50161e6596c82e6fa79e7fa662abbc66d14cc1db
AntonVoronezh <pstut@mail.ru>
2019-04-13 18:35:49 +0300

    17#2

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -14,11 +14,27 @@ export default function reducer(state = [], action) {
 			const index = state.findIndex(todo => todo.id === action.id);
 			return [...state.slice(0, index), ...state.slice(index + 1)];
 
-		case TOGGLE_TODO:
-			return [];
+        case TOGGLE_TODO:
+        return state.map(todo => {
+            if (todo.id !== action.id) {
+                return todo;
+            }
+
+            return Object.assign({}, todo, {
+                completed: !todo.completed
+            });
+        });
 
 		case EDIT_TODO:
-			return [];
+        return state.map(todo => {
+            if (todo.id !== action.id) {
+                return todo;
+            }
+
+            return Object.assign({}, todo, {
+                title: action.title
+            });
+        });
 
 		default:
 			return state;
